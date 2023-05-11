face37595b8bf6e2c790428588aa67cbd813bccf
ny <ny567@naver.com>
2023-05-11 17:32:21 +0900

    230511 수업

--- SpringWebBasic/src/main/java/com/spring/basic/controller/RequestController.java
+++ SpringWebBasic/src/main/java/com/spring/basic/controller/RequestController.java
@@ -1,6 +1,8 @@
 package com.spring.basic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,14 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //빈을 등록해 놔야 HandlerMapping이 이 클래스의 객체를 검색할 수 있을 것이다.
 @Controller
 public class RequestController {
-	
+
 	public RequestController() {
 		System.out.println("RequestCon 생성!");
 	}
 	
 	@RequestMapping("/request/test")
 	public String testCall() {
-		System.out.println("request/test 요청이 들어옴!");
+		System.out.println("/request/test 요청이 들어옴!");
 		return "test";
 	}
 	
@@ -24,15 +26,47 @@ public class RequestController {
     views폴더 아래에 request폴더 안에 존재하는
     req-ex01.jsp파일을 열도록 메서드를 구성해 보세요.
    */
-	@RequestMapping( value="/request/basic01", method = RequestMethod.GET)	
+	
+	//@RequestMapping(value = "/request/basic01", method = RequestMethod.GET)
+	@GetMapping("/request/basic01")
 	public String req() {
 		System.out.println("/request/basic01 요청이 들어옴!: GET 방식!");
-	return "request/req-ex01";
-	}
-	
-	@RequestMapping(value="/request/basic01", method = RequestMethod.POST)
-	public String basic() {
-		System.out.println("/request/basic01 요청이 들어옴!: POST 방식!");
 		return "request/req-ex01";
 	}
+	
+	//@RequestMapping(value = "/request/basic01", method = RequestMethod.POST)
+	@PostMapping("/request/basic01")
+	public String basic() {
+		System.out.println("/request/basic01 요청이 들어옴!: POST 방식");
+		return "request/req-ex01";
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
